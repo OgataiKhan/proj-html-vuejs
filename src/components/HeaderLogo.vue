ec0676324a4cb61c9a6e64f2ad5013a3e18ac881
OgataiKhan <badellino.kevin@gmail.com>
2023-12-19 15:34:04 +0100

    feat: fontawesome

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -9,6 +9,9 @@ export default {
 
 <template>
   HeaderLogo
+  <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+  <font-awesome-icon icon="fa-regular fa-circle-user" />
+  <font-awesome-icon icon="fa-brands fa-square-facebook" />
 </template>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
+
+//fontawesome core
+import { library } from '@fortawesome/fontawesome-svg-core';
+//fontawesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+//fontawesome import specific icons
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
+
 //file di partenza stili progetto
 import './assets/scss/style.scss';
 //file di partenza Vue
 import App from './App.vue';
 
-createApp(App).mount('#app');
+//fontawesome add icons to library
+library.add(faCartShopping, faCircleUser, faSquareFacebook);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');

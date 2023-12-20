315197fd4282bcceab6c0f442020d01ef49b0ef0
OgataiKhan <badellino.kevin@gmail.com>
2023-12-20 08:59:51 +0100

    feat: navbar icons

--- src/components/HeaderNavbarIcons.vue
+++ src/components/HeaderNavbarIcons.vue
@@ -8,9 +8,26 @@ export default {
 </script>
 
 <template>
-  HeaderNavbarIcons
+  <div class="navbar-icons">
+    <a href="#" class="icon-box cart-box">
+      <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+    </a>
+    <a href="#" class="icon-box">
+      <font-awesome-icon icon="fa-regular fa-circle-user" />
+    </a>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.navbar-icons {
+  display: flex;
+  gap: 20px;
+  padding-left: 7px;
+  padding-right: 20px;
 
+  .icon-box {
+    display: flex;
+    align-items: center;
+  }
+}
 </style>

ba729b9eb0047219813bc309e757fa27ad3bc32b
OgataiKhan <badellino.kevin@gmail.com>
2023-12-21 15:51:26 +0100

    feat: help section decorative circles

--- src/components/MainSectionHelp.vue
+++ src/components/MainSectionHelp.vue
@@ -1,15 +1,61 @@
 <script>
 import { store } from '../store';
 import MainSectionHelpItem from './MainSectionHelpItem.vue';
+import DecorativeCircle from './DecorativeCircle.vue';
 
 export default {
   name: 'MainSectionHelp',
   components: {
     MainSectionHelpItem,
+    DecorativeCircle,
   },
   data() {
     return {
       store,
+      circles: [
+        {
+          size: '52px',
+          borderColor: '#BCE6DF',
+          innerColor: 'transparent',
+          top: '590px',
+          left: 'calc(50% - 510px)',
+        },
+        {
+          size: '35px',
+          borderColor: '#FA7D61',
+          innerColor: '#FA7D61',
+          top: '980px',
+          left: 'calc(50% - 750px)',
+        },
+        {
+          size: '46px',
+          borderColor: '#BCE6DF',
+          innerColor: '#BCE6DF',
+          top: '630px',
+          left: 'calc(50% + 580px)',
+        },
+        {
+          size: '38px',
+          borderColor: '#E28794',
+          innerColor: '#E28794',
+          top: '1070px',
+          left: 'calc(50% + 460px)',
+        },
+        {
+          size: '54px',
+          borderColor: '#EBB860',
+          innerColor: '#EBB860',
+          top: '1230px',
+          left: 'calc(50% - 740px)',
+        },
+        {
+          size: '36px',
+          borderColor: '#AAB8F1',
+          innerColor: '#AAB8F1',
+          top: '1230px',
+          left: 'calc(50% + 690px)',
+        },
+      ],
     };
   },
 };
@@ -25,6 +71,8 @@ export default {
         <MainSectionHelpItem v-for="subject in store.subjects" :title="subject.title" :description="subject.description" :faIcon="subject.faIcon" />
       </ul>
     </div>
+    <DecorativeCircle v-for="circle in circles" :size="circle.size" :borderColor="circle.borderColor" :innerColor="circle.innerColor"
+      :top="circle.top" :left="circle.left" />
   </section>
 </template>
 
@@ -39,6 +87,7 @@ export default {
   background-position: center bottom 0;
   padding-top: 100px;
   min-height: 1088px;
+  position: relative;
   .container {
     @include container;
     .section-subtitle {
